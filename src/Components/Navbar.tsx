84ba30426717e1eae231a13340e6cb9cdd4f8498
skmayya1 <skandamayya4@gmail.com>
2025-01-04 00:45:27 +0530

    Some animations

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className=' flex justify-between items-center w-full'>
+            <h1 className="text-gray-200">skandamayya4@gmail.com</h1>
+            <div className="">
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
